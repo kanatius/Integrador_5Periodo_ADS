3f86406395b0bee6241d98a910d8a48e74afc9f9
kanatius <natanalmeidadelima@gmail.com>
2020-11-10 13:40:43 -0300

    change bootstrap and jquery to local access on website

---
+++ WebSite/resources/views/componentes/includeBootstrapAndJquery.blade.php
@@ -0,0 +1,7 @@
+<!-- JQuery -->
+<script src="../jquery/jquery-3.5.1.js"></script>
+<script src="../popper/popper.min.js"></script>
+
+<!-- Bootstrap-->
+<link rel="stylesheet" href="../bootstrap-4.0.0-dist/css/bootstrap.min.css">
+<script src="../bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
